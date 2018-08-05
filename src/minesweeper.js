a48714fddc3b7828a37f9e11d1bb0287da48822a
Yihan Xu <yihanxu01@gmail.com>
2018-08-05 19:30:33 +0800

    first commit

---
+++ src/minesweeper.js
@@ -0,0 +1,15 @@
+// hard code borderline
+const blankLine = '   |   |   ';
+console.log('This is what an empty board would look like: ');
+// demo grid
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+// hard code a guess scenario
+const guessLine = '1  |   |   ';
+const bombLine = '   | B |   ';
+console.log('This is what a board with a guess and a bomb on it would look like: ');
+// demo grid
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
