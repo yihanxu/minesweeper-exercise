37c4592a237c46731e6b4f55025457a1821184fb
Yihan Xu <yihanxu01@gmail.com>
2018-08-05 20:47:51 +0800

    backup previous version & initiate new version

---
+++ src/previous versions backup.js
@@ -0,0 +1,16 @@
+// First version, 5 Aug 2018
+// hard code borderline
+const blankLine = '   |   |   ';
+console.log('This is what an empty board would look like: ');
+// demo grid
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+// hard code a guess scenario
+const guessLine = '1  |   |   ';
+const bombLine = '   | B |   ';
+console.log('This is what a board with a guess and a bomb on it would look like: ');
+// demo grid
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
