9d0df903dd0e46c3d279872f08d622c34650d6cd
Yihan Xu <yihanxu01@gmail.com>
2018-08-05 21:46:51 +0800

    introduce a board-generating function

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,16 +1,26 @@
-// First version, 5 Aug 2018
-// hard code borderline
-const blankLine = '   |   |   ';
-console.log('This is what an empty board would look like: ');
-// demo grid
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-// hard code a guess scenario
-const guessLine = '1  |   |   ';
-const bombLine = '   | B |   ';
-console.log('This is what a board with a guess and a bomb on it would look like: ');
-// demo grid
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+// softcode version
+// generate a 3 x 3 grid
+const printBoard = (board) =>{
+	console.log('Current Board:');
+	console.log(board[0].join(' | '));
+	console.log(board[1].join(' | '));
+	console.log(board[2].join(' | '));
+
+
+
+};
+
+// create an empty array for the board
+let board = [
+	[' ', ' ', ' '],
+	[' ', ' ', ' '],
+    [' ', ' ', ' ']
+];
+
+// chech what the board looks like
+printBoard(board);
+
+// set value for player's guess
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
