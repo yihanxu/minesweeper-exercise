56b155c0e793cbb355588e6ba86cfda5b6622370
Yihan Xu <yihanxu01@gmail.com>
2018-08-06 18:00:15 +0800

    test board generating function

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,13 +1,17 @@
 // Dynamic code version 3, 6 Aug 2018
 
 // generate dynamically coded playboard
-
-const generatePlayBoard (numberOfRows, numberOfColumns){
-	let board = [],
-
-	for (i = 0, i < numberOfColumns.length, i ++){
-		for (j = 0, j < numberOfRows.length, j++){
-
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
+	for (let i = 0; i < numberOfRows; i ++){
+		const row = [];
+		for (let j = 0; j < numberOfColumns; j++){
+			row.push(' '); //set empty space for each row
 		}
+		board.push(row);
 	}
+	return board;
 };
+
+// test sample
+// console.log(generatePlayerBoard(3,3));
