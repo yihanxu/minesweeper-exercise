f289ca47628668e9311ffae937e908888b123a48
Yihan Xu <yihanxu01@gmail.com>
2018-08-06 17:42:47 +0800

    first loop

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,1 +1,13 @@
 // Dynamic code version 3, 6 Aug 2018
+
+// generate dynamically coded playboard
+
+const generatePlayBoard (numberOfRows, numberOfColumns){
+	let board = [],
+
+	for (i = 0, i < numberOfColumns.length, i ++){
+		for (j = 0, j < numberOfRows.length, j++){
+
+		}
+	}
+};
