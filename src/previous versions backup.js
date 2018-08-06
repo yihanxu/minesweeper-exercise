cb5c897b9ddfa73d3ae4d995130ce3e30bb92bfe
Yihan Xu <yihanxu01@gmail.com>
2018-08-06 15:51:41 +0800

    Initiate 3rd dynamic coding version

--- src/previous versions backup.js
+++ src/previous versions backup.js
@@ -14,3 +14,27 @@ console.log('This is what a board with a guess and a bomb on it would look like:
 console.log(guessLine);
 console.log(bombLine);
 console.log(blankLine);
+
+// Second version, partially dynamic code, 5 Aug 2018
+// generate a 3 x 3 grid
+const printBoard = (board) =>{
+	console.log('Current Board:');
+	console.log(board[0].join(' | '));
+	console.log(board[1].join(' | '));
+	console.log(board[2].join(' | '));
+};
+
+// create an empty array for the board
+let board = [
+	[' ', ' ', ' '],
+	[' ', ' ', ' '],
+    [' ', ' ', ' ']
+];
+
+// chech what the board looks like
+printBoard(board);
+
+// set value for player's guess
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
